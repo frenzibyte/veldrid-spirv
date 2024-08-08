828b5dd5433951302338e317a9c3e33840888e80
Salman Ahmed <frenzibyte@gmail.com>
2024-08-08 15:14:50 +0300

    I don't know what I'm doing but it works

--- ext/sync-shaderc.cmd
+++ ext/sync-shaderc.cmd
@@ -4,12 +4,14 @@
 python %~dp0update_shaderc_sources.py --dir %~dp0shaderc --file %~dp0known_good.json
 
 :: Android NDK 27+ need this policy set on shaderc (as well as other tools)
-ren >%~dp0shaderc\CMakeLists.txt >%~dp0shaderc\CMakeLists.tmp
+move /y %~dp0shaderc\CMakeLists.txt %~dp0shaderc\CMakeLists.tmp
 
+setlocal enableDelayedExpansion
 set p=
-for /f %%a in (%~dp0shaderc\CMakeLists.tmp) do (
-  if "!p!"=="cmake_minimum_required^(VERSION 2.8.12^)" Echo "cmake_policy^(SET CMP0057 NEW^)" >>%~dp0shaderc\CMakeLists.txt
-  Echo %%a >>%~dp0shaderc\CMakeLists.tmp
+for /f "tokens=* delims=" %%a in (%~dp0shaderc\CMakeLists.tmp) do (
+  if "!p!"=="cmake_minimum_required(VERSION 2.8.12)" echo cmake_policy(SET CMP0057 NEW^)>>%~dp0shaderc\CMakeLists.txt
+  (echo %%a) >>%~dp0shaderc\CMakeLists.txt
   set p=%%a
 )
 del %~dp0shaderc\CMakeLists.tmp
+
