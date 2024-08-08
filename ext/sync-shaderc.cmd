c17120beaa3032a070bf75e7bb9e4fd567454e6b
Salman Ahmed <frenzibyte@gmail.com>
2024-08-08 14:58:36 +0300

    test

--- ext/sync-shaderc.cmd
+++ ext/sync-shaderc.cmd
@@ -5,9 +5,10 @@ python %~dp0update_shaderc_sources.py --dir %~dp0shaderc --file %~dp0known_good.
 
 :: Android NDK 27+ need this policy set on shaderc (as well as other tools)
 ren >%~dp0shaderc\CMakeLists.txt >%~dp0shaderc\CMakeLists.tmp
+
 set p=
 for /f %%a in (%~dp0shaderc\CMakeLists.tmp) do (
-  if "!p!"=="cmake_minimum_required^(VERSION 2.8.12^)" Echo "cmake_policy^(SET CMP0057 NEW^)" >> %~dp0shaderc\CMakeLists.tmp
+  if "!p!"=="cmake_minimum_required^(VERSION 2.8.12^)" Echo "cmake_policy^(SET CMP0057 NEW^)" >>%~dp0shaderc\CMakeLists.txt
   Echo %%a >>%~dp0shaderc\CMakeLists.tmp
   set p=%%a
 )
