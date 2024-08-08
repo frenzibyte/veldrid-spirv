26e0bcfe600687f79c834ce6e8adfba4dffb21e5
Salman Ahmed <frenzibyte@gmail.com>
2024-08-08 14:37:30 +0300

    Fix shaderc not having policy set as well

--- ext/sync-shaderc.cmd
+++ ext/sync-shaderc.cmd
@@ -2,3 +2,9 @@
 @echo off
 
 python %~dp0update_shaderc_sources.py --dir %~dp0shaderc --file %~dp0known_good.json
+
+:: Android NDK 27+ need this policy set on shaderc (as well as other tools)
+echo cmake_policy(SET CMP0057 NEW^) >%~dp0shaderc\CMakeLists.txt.new
+type %~dp0shaderc\CMakeLists.txt >>%~dp0shaderc\CMakeLists.txt.new
+move /y %~dp0shaderc\CMakeLists.txt.new %~dp0shaderc\CMakeLists.txt
+cat %~dp0shaderc\CMakeLists.txt
